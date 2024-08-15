86722c8047e3fb329357ccb944390fafee3c0a87
Mark Santoro <mark_santoro@comcast.net>
2024-08-15 13:26:30 -0700

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Mark Santoro on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

50016938b351337c7df327b7d2e48f2bf7b58062
Mark Santoro <mark_santoro@comcast.net>
2024-08-16 09:00:40 -0700

    added third view file

---
+++ FirstSwiftUIApp/ThirdView.swift
@@ -0,0 +1,39 @@
+//
+//  ThirdView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Mark Santoro on 8/16/24.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    
+    let myArray = ["Homer", "Bart", "Marge", "Moe"]
+    
+    var body: some View {
+        // manually make list identifiers
+            List (myArray, id: \.self) { element in
+                Image(myArray[0])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                Text(element).font(.largeTitle)
+            
+        }
+        
+//       List {
+//            ForEach(myArray, id: \.self) { element in
+//             
+//                Text(element)
+//                
+//            }
+//        }
+        
+        
+    }
+}
+
+#Preview {
+    ThirdView()
+}
